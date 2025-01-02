15718393f8569695ca5bd9dbab8a58fcd13e7566
Nahom Worku <nahombekalu12@gmail.com>
2025-01-01 21:58:21 -0500

    File names and folders update

---
+++ AccessEd/AccessEd/Views/OnboardingViews/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  AccessEd
+//
+//  Created by Nahom Worku on 2024-12-31.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
---
+++ AccessEd/AccessEd/Views/OnboardingViews/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  AccessEd
+//
+//  Created by Nahom Worku on 2024-12-30.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
