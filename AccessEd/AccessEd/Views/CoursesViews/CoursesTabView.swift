93976e88654a41128d310e5bea87a0b67b098a1f
Nahom Worku <nahombekalu12@gmail.com>
2025-02-11 02:12:22 -0500

    Delete study cards logic added

--- AccessEd/AccessEd/Views/CoursesViews/CoursesTabView.swift
+++ AccessEd/AccessEd/Views/CoursesViews/CoursesTabView.swift
@@ -9,13 +9,9 @@ import SwiftUI
 import SwiftData
 
 struct CoursesTabView: View {
-    
     var modelContext: ModelContext
     var course: CourseModel
-    
     @State var showAddStudyCard: Bool = false
-//    @State var studyCardName: String = ""
-//    @FocusState private var isTaskFieldFocused: Bool
     @Environment(\.dismiss) var dismiss
     
     
@@ -24,7 +20,7 @@ struct CoursesTabView: View {
             List {
                 Section(header: Text(course.studyCards.isEmpty ? "" : "Study Cards")) {
                     ForEach(course.distinctStudyCardNames, id: \.self) { cardName in
-                        NavigationLink(destination: StudyCardsView(modelContext: modelContext, course: course, studyCardName: cardName) ){ // course: $course/*, cardNumber: 5*/)
+                        NavigationLink(destination: StudyCardsView(modelContext: modelContext, course: course, studyCardName: cardName) ){
                             RoundedRectangle(cornerRadius: 10)
                                 .fill(course.courseColor) //Color("StudyCard-Colors"))
                                 .padding(.horizontal, 25)
@@ -55,39 +51,8 @@ struct CoursesTabView: View {
                         }
                         .listRowBackground(Color("Light-Dark Mode Colors"))
                     }
-//                    .onDelete(perform: deleteStudyCardGroup)
+                    .onDelete(perform: deleteStudyCard)
                 }
-                
-//                Section(header: Text("Quiz")) {
-//                    NavigationLink(destination: QuizView()) {
-//                        RoundedRectangle(cornerRadius: 10)
-//                            .fill(Color("StudyCard-Colors")) //course.courseColor)
-//                            .padding(.horizontal, 30)
-//                            .padding(.leading)
-//                            .frame(width: UIScreen.main.bounds.width, height: 75)
-//                            .shadow(radius: 1, x: 0, y: 1)
-//                            .overlay(
-//                                HStack {
-//                                    Image(systemName: "rectangle.fill.on.rectangle.angled.fill")
-//                                        .resizable()
-//                                        .frame(width: 40, height: 40)
-//                                        .foregroundStyle(Color.gray.opacity(0.5)) //Color("StudyCard-Colors"))
-//                                        .scaledToFit()
-//                                        .padding()
-//                                    
-//                                    Text("Quiz")
-//                                        .font(.headline)
-//                                        .foregroundStyle(Color("Text-Colors"))
-//                                    
-//                                    Spacer()
-//                                    
-//                                    Image(systemName: "chevron.right")
-//                                }
-//                                    .padding(.horizontal, 60)
-//                            )
-//                    }
-//                    .listRowBackground(Color("Light-Dark Mode Colors"))
-//                }
                 .navigationTitle(course.name)
             }
             .listStyle(InsetGroupedListStyle())
@@ -95,48 +60,8 @@ struct CoursesTabView: View {
             .background(Color("Light-Dark Mode Colors"))
             
             
-            if !course.studyCards.isEmpty {
-                addCourseButton
-            } else {
-//                VStack {
-//                    Text("Add a study card to get started!")
-//                        .font(.headline)
-//                    
-//                    Button("Add Study Card") {
-//                        showAddStudyCard = true
-//                    }
-//                    .buttonStyle(.borderedProminent)
-//                }
-                
-                VStack(alignment: .center) {
-                    Image(systemName: "rectangle.stack.fill")
-                        .font(Font.system(size: 60))
-                        .padding(5)
-                        .foregroundStyle(.gray.opacity(0.8))
-                        .fontWeight(.light)
-                    
-                    Text("No Study Cards")
-                        .font(.title2)
-                        .bold()
-                        .opacity(0.8)
-                        .padding(.bottom, 5)
-                    
-                    Text("Start adding Study Cards to get started!")
-                        .font(.subheadline)
-                        .foregroundStyle(.gray)
-                        .padding(.bottom)
-                        .padding(.horizontal, 50)
-                        .multilineTextAlignment(.center)
-                    
-                    
-                    Button(action: {
-                        showAddStudyCard = true
-                    }, label: {
-                        Text("Add Study Card")
-                            .font(.headline)
-                    })
-                }
-            }
+            if !course.studyCards.isEmpty { addCourseButton }
+            else { emptyStudyCardsView }
         }
         .sheet(isPresented: $showAddStudyCard) {
             CreateStudyCardsView(modelContext: modelContext, course: course)
@@ -150,9 +75,7 @@ struct CoursesTabView: View {
             HStack {
                 Spacer()
                 
-                // add courses button
                 Button(action: {
-//                    showAddCoursesBottomView = true
                     showAddStudyCard = true
                 }) {
                     Circle()
@@ -170,7 +93,37 @@ struct CoursesTabView: View {
                 .padding(.vertical)
                 
             }
-//            .padding(.trailing)
+        }
+    }
+    
+    var emptyStudyCardsView: some View {
+        VStack(alignment: .center) {
+            Image(systemName: "rectangle.stack.fill")
+                .font(Font.system(size: 60))
+                .padding(5)
+                .foregroundStyle(.gray.opacity(0.8))
+                .fontWeight(.light)
+            
+            Text("No Study Cards")
+                .font(.title2)
+                .bold()
+                .opacity(0.8)
+                .padding(.bottom, 5)
+            
+            Text("Start adding Study Cards to get started!")
+                .font(.subheadline)
+                .foregroundStyle(.gray)
+                .padding(.bottom)
+                .padding(.horizontal, 50)
+                .multilineTextAlignment(.center)
+            
+            
+            Button(action: {
+                showAddStudyCard = true
+            }, label: {
+                Text("Add Study Card")
+                    .font(.headline)
+            })
         }
     }
     
@@ -180,9 +133,15 @@ struct CoursesTabView: View {
     
     
     // TODO: delete study card
+    private func deleteStudyCard(at offsets: IndexSet) {
 
+        for index in offsets {
+            let cardNameToDelete = course.distinctStudyCardNames[index]
+            let cardsToDelete = course.studyCards.filter { $0.studyCardName == cardNameToDelete }
+            
+            for card in cardsToDelete { modelContext.delete(card) }
+            try? modelContext.save()
+        }
+    }
 }
 
-//#Preview {
-//    CoursesTabView()
-//}
